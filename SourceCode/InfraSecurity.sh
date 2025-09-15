9027fc3f7a0d6fef097ddb8123d717352adfba95
Donghyun Choi <101495051+NullBins@users.noreply.github.com>
2025-09-15 14:17:25 +0900

    📑 Source code (Assignment 1)

--- SourceCode/InfraSecurity.sh
+++ SourceCode/InfraSecurity.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 ## ----------------------------------------------------------------------- ##
 docker network create -d macvlan --subnet 192.168.127.0/24 --gateway 192.168.127.1 -o parent=ens33 kolo-net
-nano /etc/rc.local
-# --- #
+nano /etc/rc.local > /dev/null << EOF
 #!/bin/bash
 sysctl --system
 # Virtual Network Bridge
@@ -14,18 +13,13 @@ ip route add 192.168.127.20/32 dev kolo-net-host
 ip addr show kolo-net-host | grep inet
 ip route show | grep kolo-net
 exit 0
-# --- #
+EOF
 chmod +x /etc/rc.local
 systemctl restart rc-local
-nano /etc/mysql/conf.d/mysql.cnf
-# --- #
-[mysqld]
-lower_case_table_names=1
-# --- #
 mysqldump -u root -p midsv > ./midsv_backup.sql
 mysqldump -u root -p mysql_bk > ./mysql_bk_backup.sql
 docker tag mariadb:latest mariadb:kolo
-docker run -d --name kolodb --hostname kolodb --restart always --network kolo-net --ip 192.168.127.10 -e MARIADB_ROOT_PASSWORD=user01 -v /etc/mysql/conf.d/mysql.cnf:/etc/mysql/conf.d/custom.cnf mariadb:kolo
+docker run -d --name kolodb --hostname kolodb --restart always --network kolo-net --ip 192.168.127.10 -e MARIADB_ROOT_PASSWORD=user01 mariadb:kolo --lower_case_table_names=1
 mariadb -h 192.168.127.10 -u root -puser01 -e "grant all privileges on *.* to 'dbroot'@'%' identified by 'asd123'"
 mariadb -h 192.168.127.10 -u root -puser01 -e 'flush privileges'
 mariadb -h 192.168.127.10 -u root -puser01 -e 'create database midsv'
@@ -38,15 +32,14 @@ sed -i "s/localhost/192.168.127.10/g" /home/kolo_user/apache-tomcat-9.0.89/webap
 /home/kolo_user/apache-tomcat-9.0.89/bin/shutdown.sh
 /home/kolo_user/apache-tomcat-9.0.89/bin/startup.sh
 cd /home/kolo_user/
-nano Dockerfile
-# --- #
+tee Dockerfile > /dev/null << EOF
 FROM tomcat:9.0
 COPY ./apache-tomcat-9.0.89/ /usr/local/tomcat/
-# --- #
+EOF
 docker build -t tomcat:kolo .
-docker run -d --name www --hostname www --restart always --network kolo-net --ip 192.168.127.20 tomcat:kolo
 sed -i "s/192.168.127.10/localhost/g" /home/kolo_user/apache-tomcat-9.0.89/webapps/midsv/WEB-INF/spring/root-context.xml
 sed -i "s/192.168.127.10/localhost/g" /home/kolo_user/apache-tomcat-9.0.89/webapps/midsv/WEB-INF/classes/config/value.properties
 /home/kolo_user/apache-tomcat-9.0.89/bin/shutdown.sh
 /home/kolo_user/apache-tomcat-9.0.89/bin/startup.sh
+docker run -d --name www --hostname www --restart always --network kolo-net --ip 192.168.127.20 tomcat:kolo
 ## ----------------------------------------------------------------------- ##
